01a06d6bf392f2941695260678770ac0b559f6f6
akashkhot12 <akashkhot03@gmail.com>
2024-06-01 11:36:06 +0530

    adding new package

--- express_js/crud_Mongo/server.js
+++ express_js/crud_Mongo/server.js
@@ -1,10 +1,17 @@
-const express = require("express");
-const app = express();
+const express = require('express');
+const {MongoClient}= require("mongodb");
+const url  = "mongodb://localhost:27017";
+const database = "Backend_operations";
 
-app.get('/',(req,res)=>{
-    res.send("hello world")
-})
+const client = new MongoClient(url);
 
-app.listen(3000,()=>{
-    console.log("server is started ");
-})
+
+let getdata = async()=>{
+    let result = await db.client.connect();
+    let    = result.db(database);
+    let collections = db.collections("crud_mongo");
+    console.log(collections.find().toArray());
+
+}
+
+getdata();
